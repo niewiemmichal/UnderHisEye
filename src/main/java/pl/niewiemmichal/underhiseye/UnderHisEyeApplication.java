4eee4b0b06699c1d15c1247aaa6f3761e8ccb103
Michał Padula <m.padula@onet.pl>
2019-04-05 18:12:57 +0200

    Initial commit

---
+++ src/main/java/pl/niewiemmichal/underhiseye/UnderHisEyeApplication.java
@@ -0,0 +1,13 @@
+package pl.niewiemmichal.underhiseye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnderHisEyeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnderHisEyeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/niewiemmichal/underhiseye/UnderHisEyeApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.niewiemmichal.underhiseye;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UnderHisEyeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
