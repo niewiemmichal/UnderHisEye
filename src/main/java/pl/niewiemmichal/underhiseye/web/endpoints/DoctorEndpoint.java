0ed5b724e916a6bff95361c6d223f22912eb50bc
Michał Granda <majcoch98@gmail.com>
2019-04-11 19:35:50 +0200

    Create doctor endpoint and implement basic errors with exception classes

--- src/main/java/pl/niewiemmichal/underhiseye/web/endpoints/DoctorEndpoint.java
+++ src/main/java/pl/niewiemmichal/underhiseye/web/endpoints/DoctorEndpoint.java
@@ -1,34 +1,49 @@
 package pl.niewiemmichal.underhiseye.web.endpoints;
+import pl.niewiemmichal.underhiseye.commons.exceptions.ResourceDoesNotExistException;
+import pl.niewiemmichal.underhiseye.commons.exceptions.ResourceConflictException;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import pl.niewiemmichal.underhiseye.model.Doctor;
+import pl.niewiemmichal.underhiseye.repository.DoctorRepository;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("doctors")
+@ResponseBody
 public class DoctorEndpoint {
 
+    private final DoctorRepository doctorRepository;
+
+    DoctorEndpoint(DoctorRepository doctorRepository) {
+        this.doctorRepository = doctorRepository;
+    }
+
     @GetMapping("/{id}")
-    @ResponseBody
-    public String getDoctor(@PathVariable Integer id){
-        return id.toString();
+    public Doctor getDoctor(@PathVariable Long id){
+        return doctorRepository.findById(id).orElseThrow(() -> new ResourceDoesNotExistException("Doctor","id",id.toString()));
     }
 
     @GetMapping
-    @ResponseBody
-    public String getAllDoctors(){
-        return "Get all doctors";
+    public List<Doctor> getAllDoctors(){
+        return (List<Doctor>) doctorRepository.findAll();
     }
 
     @PostMapping
-    @ResponseBody
-    public String addDoctor(){
-        return "Add doctor";
+    public Doctor addDoctor(@RequestBody Doctor newDoctor){
+        return doctorRepository.save(newDoctor);
     }
 
     @PutMapping("/{id}")
-    @ResponseBody
-    public String updateDoctor(@PathVariable Integer id){
-        return "Update doctor" + id.toString();
+    public Doctor updateDoctor(@RequestBody Doctor newDoctor, @PathVariable Long id){
+        if(!doctorRepository.findById(id).isPresent())
+            throw new ResourceDoesNotExistException("Question", "id", id.toString());
+        else if(newDoctor.getId() != null && !(id.equals(newDoctor.getId())))
+            throw new ResourceConflictException("Question", "id", id.toString(), newDoctor.getId().toString());
+        else {
+            newDoctor.setId(id);
+            return doctorRepository.save(newDoctor);
+        }
     }
 }
