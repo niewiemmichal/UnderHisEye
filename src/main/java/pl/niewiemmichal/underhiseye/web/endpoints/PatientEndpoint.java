bebcba902624750136c97a84b87563c5fcea337e
Claudia Polaczek <claudia.polaczek@gmail.com>
2019-04-12 00:24:44 +0200

    Create Endpoint for Patient

---
+++ src/main/java/pl/niewiemmichal/underhiseye/web/endpoints/PatientEndpoint.java
@@ -0,0 +1,51 @@
+package pl.niewiemmichal.underhiseye.web.endpoints;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import pl.niewiemmichal.underhiseye.commons.exceptions.ResourceConflictException;
+import pl.niewiemmichal.underhiseye.commons.exceptions.ResourceDoesNotExistException;
+import pl.niewiemmichal.underhiseye.model.Patient;
+import pl.niewiemmichal.underhiseye.repository.PatientRepository;
+
+import java.util.List;
+
+@Controller
+@RequestMapping ("patients")
+@ResponseBody
+public class PatientEndpoint {
+
+    private final PatientRepository patientRepository;
+
+    PatientEndpoint(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    @GetMapping ("/{id}")
+    public Patient getPatient(@PathVariable Long id){
+        return patientRepository.findById(id).orElseThrow(()
+                -> new ResourceDoesNotExistException("Address","id",id.toString()));
+    }
+
+    @GetMapping
+    public List<Patient> getAllPatients(){
+        return (List<Patient>) patientRepository.findAll();
+    }
+
+    @PostMapping
+    public Patient addPatients(@RequestBody Patient newPatient){
+        return patientRepository.save(newPatient);
+    }
+
+    @PutMapping("/{id}")
+    public Patient updatePatient(@RequestBody Patient newPatient, @PathVariable Long id) {
+        if(!patientRepository.findById(id).isPresent())
+            throw new ResourceDoesNotExistException("Patient", "id", id.toString());
+        else if(newPatient.getId() != null && !(id.equals(newPatient.getId())))
+            throw new ResourceConflictException("Patient", "id", id.toString(), newPatient.getId().toString());
+        else {
+            newPatient.setId(id);
+            return patientRepository.save(newPatient);
+        }
+    }
+
+}
