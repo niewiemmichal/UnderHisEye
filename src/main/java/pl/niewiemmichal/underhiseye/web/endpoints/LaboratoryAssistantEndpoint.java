ee15f465b0e06f427f3deb427917ba08b624d0fc
Claudia Polaczek <claudia.polaczek@gmail.com>
2019-04-11 20:11:34 +0200

    Create Endpoint for Address and LaboratoryAssistant

---
+++ src/main/java/pl/niewiemmichal/underhiseye/web/endpoints/LaboratoryAssistantEndpoint.java
@@ -0,0 +1,49 @@
+package pl.niewiemmichal.underhiseye.web.endpoints;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import pl.niewiemmichal.underhiseye.commons.exceptions.*;
+import pl.niewiemmichal.underhiseye.model.LaboratoryAssistant;
+import pl.niewiemmichal.underhiseye.repository.LaboratoryAssistantRepository;
+
+import java.util.List;
+
+@Controller
+@RequestMapping ("laboratoryassistants")
+@ResponseBody
+public class LaboratoryAssistantEndpoint {
+
+    private final LaboratoryAssistantRepository laboratoryAssistantRepository;
+
+    LaboratoryAssistantEndpoint(LaboratoryAssistantRepository laboratoryAssistantRepository) {
+        this.laboratoryAssistantRepository = laboratoryAssistantRepository;
+    }
+
+    @GetMapping ("/{id}")
+    public LaboratoryAssistant getLaboratoryAssistant(@PathVariable Long id){
+        return laboratoryAssistantRepository.findById(id).orElseThrow(()
+                -> new ResourceDoesNotExistException("LaboratoryAssistant","id",id.toString()));
+    }
+
+    @GetMapping
+    public List<LaboratoryAssistant> getAllLaboratoryAssistants(){
+        return (List<LaboratoryAssistant>) laboratoryAssistantRepository.findAll();
+    }
+
+    @PostMapping
+    public LaboratoryAssistant addLaboratoryAssistant(@RequestBody LaboratoryAssistant newLaboratoryAssistant){
+        return laboratoryAssistantRepository.save(newLaboratoryAssistant);
+    }
+
+    @PutMapping("/{id}")
+    public LaboratoryAssistant updateLaboratoryAssistant(@RequestBody LaboratoryAssistant newLaboratoryAssistant, @PathVariable Long id){
+        if(!laboratoryAssistantRepository.findById(id).isPresent())
+            throw new ResourceDoesNotExistException("LaboratoryAssistant", "id", id.toString());
+        else if(newLaboratoryAssistant.getId() != null && !(id.equals(newLaboratoryAssistant.getId())))
+            throw new ResourceConflictException("LaboratoryAssistant", "id", id.toString(), newLaboratoryAssistant.getId().toString());
+        else {
+            newLaboratoryAssistant.setId(id);
+            return laboratoryAssistantRepository.save(newLaboratoryAssistant);
+        }
+    }
+}
