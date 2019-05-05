9bbce8c6e63f0efa2000724139854427ff3251ac
Michał Padula <m.padula@onet.pl>
2019-05-05 13:21:22 +0200

    Add missing annotation

--- src/test/java/pl/niewiemmichal/underhiseye/services/DefaultVisitServiceTest.java
+++ src/test/java/pl/niewiemmichal/underhiseye/services/DefaultVisitServiceTest.java
@@ -16,6 +16,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import pl.niewiemmichal.underhiseye.commons.dto.*;
@@ -29,6 +30,7 @@ import pl.niewiemmichal.underhiseye.repositories.PatientRepository;
 import pl.niewiemmichal.underhiseye.repositories.VisitRepository;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class DefaultVisitServiceTest
 {
 
