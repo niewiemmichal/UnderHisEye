15d437ef4b725242a43c1a7a1a6daad67a08651f
Michał Padula <m.padula@onet.pl>
2019-05-10 17:31:51 +0200

    Change swagger config

--- src/main/java/pl/niewiemmichal/underhiseye/web/configurations/SwaggerConfiguration.java
+++ src/main/java/pl/niewiemmichal/underhiseye/web/configurations/SwaggerConfiguration.java
@@ -7,10 +7,9 @@ import com.google.common.collect.Lists;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.BasicAuth;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -22,10 +21,28 @@ public class SwaggerConfiguration {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).select()
                 .apis(RequestHandlerSelectors.basePackage("pl.niewiemmichal.underhiseye.web.endpoints"))
-                .paths(PathSelectors.regex("/.*"))
-                .build()
+                .paths(PathSelectors.regex("/.*")).build()
                 .apiInfo(apiEndPointsInfo())
-                .securitySchemes(Lists.newArrayList(new BasicAuth("basicAuth")));
+                .securitySchemes(Lists.newArrayList(securityScheme()))
+                .securityContexts(Lists.newArrayList(securityContext()));
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(Lists.newArrayList(securityReference()))
+                .forPaths(PathSelectors.any()).build();
+    }
+
+    private SecurityReference securityReference() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return new SecurityReference("basicAuth", authorizationScopes);
+    }
+
+    private SecurityScheme securityScheme() {
+        return new BasicAuth("basicAuth");
     }
 
     private ApiInfo apiEndPointsInfo() {
