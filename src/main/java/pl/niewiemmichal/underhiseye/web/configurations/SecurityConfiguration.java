4b6605f6742c7b6ef66d2f705e68facca2ed1e9e
Michał Padula <m.padula@onet.pl>
2019-05-11 01:16:24 +0200

    Disable cors

--- src/main/java/pl/niewiemmichal/underhiseye/web/configurations/SecurityConfiguration.java
+++ src/main/java/pl/niewiemmichal/underhiseye/web/configurations/SecurityConfiguration.java
@@ -1,6 +1,8 @@
 package pl.niewiemmichal.underhiseye.web.configurations;
 
+import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,7 +10,9 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -17,11 +21,23 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().httpBasic();
+        http.cors().and().httpBasic().and().csrf().disable();
     }
 
     @Autowired
     protected void configure(AuthenticationManagerBuilder auth, DaoAuthenticationProvider provider) {
         auth.authenticationProvider(provider);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Lists.newArrayList("Authorization"));
+        configuration.setAllowedOrigins(Lists.newArrayList("*"));
+        configuration.setAllowedMethods(Lists.newArrayList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
